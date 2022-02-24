092527b9c74a1c6d5af0ea8aa6e539638df75be1
Gina Henderson <gina@ginahenderson.me>
2022-02-24 01:12:17 -0600

    Fix grid sizing

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 // Create 16 x 16 grid using divs
 const wrapper = document.querySelector('.wrapper');
 const container = document.querySelector('.container');
-let pixel = [];
-for (let i = 0; i < (16 * 16); i++) {
-    pixel[i] = document.createElement('div');
-    pixel[i].classList.add('pixel');
-    pixel[i].style.border = "solid 1px blue";
-    container.appendChild(pixel[i]);
-}
 
+let pixel = [];
+
+function createGrid(gridsize) {
+    pixel = [];
+    for (let i = 0; i < (gridsize * gridsize); i++) {
+        pixel[i] = document.createElement('div');
+        pixel[i].classList.add('pixel');
+        pixel[i].style.border = "solid 1px blue";
+        container.appendChild(pixel[i]);
+    }
+    container.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridsize}, 1fr)`;
 // Add "hover" effect
-pixel.forEach(a => a.addEventListener('mouseover', setColor.bind(a, 'black')));
+    pixel.forEach(a => a.addEventListener('mouseover', setColor.bind(a, 'black')));
+}
+createGrid(setGridSize());
 
 function setColor(color) {
     this.style.backgroundColor = color;
 }
+
 // Add "clear grid" button at top of screen
 const clearButton = document.createElement('button');
 clearButton.textContent = "Clear Grid";
 clearButton.addEventListener('click', clearGrid)
 wrapper.insertBefore(clearButton, container);
 
+// Allow user input (prompt) for grid size (>= 100 x 100)
+    function setGridSize() {
+    do { gridSize = parseInt(prompt('What size grid? (Between 16 and 100)', 16)); }
+    while (gridSize < 16 || gridSize > 100);
+    return gridSize;
+}
 function clearGrid() {
     pixel.forEach(a => a.style.backgroundColor = 'transparent');
+    createGrid(setGridSize());
 }
 
-// Allow user input (prompt) for grid size (>= 100 x 100)
 
 // Stay within 960px wide
 // OPTIONAL: Each pass of mouse adds 10% black to color, after 10 passes the square is black
